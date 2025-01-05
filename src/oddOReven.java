654a633e94949d6cbd58dc1085e991b343336e41
gautam-kumar7372 <gautamkumarbihar948@gmail.com>
2025-01-06 01:15:51 +0530

    day2DSA

--- src/oddOReven.java
+++ src/oddOReven.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class oddOReven {
+public class oddOReven{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a value");
