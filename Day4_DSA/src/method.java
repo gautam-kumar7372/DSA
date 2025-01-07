901e92d7b6f9b01c49933de0d03dba9f9229627a
gautam-kumar7372 <gautamkumarbihar948@gmail.com>
2025-01-08 01:49:22 +0530

    day4DSA

---
+++ Day4_DSA/src/method.java
@@ -0,0 +1,12 @@
+public class method {
+    public static void main(String[] args) {
+
+      System.out.println(sum());
+    }
+    static int sum(){
+        int sum=0;
+        int a=5;
+        int b=10;
+        return sum=a*b;
+    }
+}
